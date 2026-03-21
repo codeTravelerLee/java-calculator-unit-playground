b0eedec3204661f2c9ec19287314d80f8e831e48
RomanticGuy <109668122+codeTravelerLee@users.noreply.github.com>
2026-03-21 07:58:35 +0000

    feat: 문자열에서 숫자를 추출하여 합산하는 기능 및 테스트코드 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,19 +1,51 @@
 package calculator;
 
+import java.util.regex.Pattern;
+
 public class Calculator {
-    int add(int n1, int n2) {
+     //문자열을 전달받아 구분자를 기준으로 분리한 수의 합을 반환
+    public int sumFromString(String inputString) {
+        if(inputString == null || inputString.isBlank()) return 0;
+
+        int sum = 0;
+        String separator = "[,:]";
+
+        //커스텀 구분자인 경우 구분자, 입력문자열 재할당 
+        if(inputString.startsWith("//")) {
+            int newLineIdx = inputString.indexOf("\n");
+            separator = Pattern.quote(inputString.substring(2, newLineIdx));
+
+            inputString = inputString.substring(newLineIdx + 1);
+        }
+
+        for(String s : inputString.split(separator)) {
+            try {
+                int num = Integer.parseInt(s.trim());
+                if(num < 0) throw new RuntimeException("음수값이 입력될 순 없어요!");
+
+                sum += num; 
+            } catch (Exception e) {
+                throw new RuntimeException("숫자 이외의 값이 들어있으면 안돼요!");
+            }            
+        }
+
+        return sum;
+    }
+
+    public int add(int n1, int n2) {
         return n1 + n2;
     }
 
-    int subtract(int n1, int n2) {
+    public int subtract(int n1, int n2) {
         return n1 - n2;
     }
 
-    int multiply(int n1, int n2) {
+    public int multiply(int n1, int n2) {
         return n1 * n2;
     }
 
-    int division(int n1, int n2) {
+    public int division(int n1, int n2) {
         return n1 / n2;
     }
+
 }
